97d7bf1c576cd0991cd644b5444818f5ba84aa89
Nissim Albagli <nissimalbagli@gmail.com>
2024-01-26 00:11:25 -0300

    Componente de Alerta y state para almacenar y utilizar informacion de la API

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
+  import { ref, reactive ,onMounted } from 'vue';
+  import Alerta from './components/Alerta.vue';
 
   const monedas = ref([
       { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
@@ -10,13 +11,30 @@
   ]);
 
   const criptomonedas = ref([]);
+  const error = ref('')
+
+  const cotizar = reactive({
+    moneda: '',
+    criptomoneda: '',
+  })
 
   onMounted(() => {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
     fetch(url)
       .then(respuesta => respuesta.json())
       .then(({ Data }) => criptomonedas.value = Data)
-  })
+  });
+
+  const cotizarCripto = () => {
+    // Validar que cotizar este lleno
+    if(Object.values(cotizar).includes('')) {
+      error.value = 'Todos los campos son obligatorios'
+      return
+    }
+    error.value = '';
+    
+    console.log('Cotiazndo...');
+  }
 </script>
 
 <template>
@@ -24,20 +42,47 @@
     <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
 
     <div class="contenido">
-      <form class="formulario">
 
-      <div class="campo">
-        <label for="moneda">Moneda:</label>
-        <select id="moneda">
-          <option value="">-- Selecciona --</option>
-          <option 
-            v-for="moneda in monedas" 
-            :value="moneda.codigo">
-            {{  moneda.texto }}</option>
-        </select>
+      <Alerta
+        v-if="error"
+      >{{  error  }}</Alerta>
 
-      </div>
-    </form>
+       <form 
+       class="formulario"
+       @submit.prevent="cotizarCripto"
+       >
+
+       <div class="campo">
+         <label for="moneda">Moneda:</label>
+         <select 
+           id="moneda"
+           v-model="cotizar.moneda"
+           >
+           <option value="">-- Selecciona --</option>
+           <option 
+             v-for="moneda in monedas" 
+             :value="moneda.codigo">
+             {{  moneda.texto }}</option>
+         </select>
+       </div>
+
+       <div class="campo">
+         <label for="cripto">Criptomoneda:</label>
+         <select 
+           id="cripto"
+           v-model="cotizar.criptomoneda"
+           >
+           <option value="">-- Selecciona --</option>
+           <option 
+             v-for="criptomoneda in criptomonedas" 
+             :value="criptomoneda.CoinInfo.Name">
+             {{  criptomoneda.CoinInfo.FullName }}</option>
+         </select>
+
+       </div>
+
+       <input type="submit" value="Cotizar"/>
+     </form>
     </div>
   </div>
 </template>
