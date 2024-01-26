ae811cb47e1450cb3364a3209aef3b530a925f27
Nissim Albagli <nissimalbagli@gmail.com>
2024-01-25 23:41:11 -0300

    Fetch API cripto compare y guardar los datos en un array

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref } from 'vue';
+  import { ref, onMounted } from 'vue';
 
   const monedas = ref([
       { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
@@ -7,7 +7,16 @@
       { codigo: 'EUR', texto: 'Euro'},
       { codigo: 'GBP', texto: 'Libra Esterlina'},
       { codigo: 'CLP', texto: 'Peso Chileno'},
-  ])
+  ]);
+
+  const criptomonedas = ref([]);
+
+  onMounted(() => {
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(({ Data }) => criptomonedas.value = Data)
+  })
 </script>
 
 <template>
